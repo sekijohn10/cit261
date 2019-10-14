293c201b599df62f1036d15ab361e79c843740f4
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-10-14 10:08:39 -0600

    adjusted how special character is added to queryLabel

--- Script1.js
+++ Script1.js
@@ -1,16 +1,16 @@
 function updateQuery() {
     let topic = document.getElementById("queryTopic").value;
     if (topic == "pokemon") {
-        document.getElementById("queryLabel").innerHTML = "Pokémon's name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon's name or id:";
     }
     else if (topic == "move") {
-        document.getElementById("queryLabel").innerHTML = "Pokémon moves by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon moves by name or id:";
     }
     else if (topic == "ability") {
-        document.getElementById("queryLabel").innerHTML = "Pokémon abilities by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon abilities by name or id:";
     }
     else {
-        document.getElementById("queryLabel").innerHTML = "Pokémon types by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon types by name or id:";
     }
 }
 
