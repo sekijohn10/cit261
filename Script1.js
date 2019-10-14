44fc6875491688c4f7388b7d9e6a1c97169168ae
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-10-14 10:00:46 -0600

    Added updateQuery function, adjusted spelling, added id to element

--- Script1.js
+++ Script1.js
@@ -1,5 +1,17 @@
 function updateQuery() {
-
+    let topic = document.getElementById("queryTopic").value;
+    if (topic == "pokemon") {
+        document.getElementById("queryLabel").innerHTML = "Pokémon's name or id:";
+    }
+    else if (topic == "move") {
+        dcoument.getElementById("queryLabel").innerHTML = "Pokémon moves by name or id:";
+    }
+    else if (topic == "ability") {
+        dcoument.getElementById("queryLabel").innerHTML = "Pokémon abilities by name or id:";
+    }
+    else {
+        document.getElementById("queryLabel").innerHTML = "Pokémon types by name or id:";
+    }
 }
 
 function clearPage() {
