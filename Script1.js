92a2e1b08d03adbe131bded3f97e46e2afac87a5
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-10-14 10:14:23 -0600

    changed wording for clarity

--- Script1.js
+++ Script1.js
@@ -4,13 +4,13 @@ function updateQuery() {
         document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon's name or id:";
     }
     else if (topic == "move") {
-        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon moves by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon move by name or id:";
     }
     else if (topic == "ability") {
-        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon abilities by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon ability by name or id:";
     }
     else {
-        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon types by name or id:";
+        document.getElementById("queryLabel").innerHTML = "Pok\u00E9mon type by name or id:";
     }
 }
 
