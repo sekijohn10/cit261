672362d9254eca4e13f1a4ea4c923fbcb14c3402
sekijohn10 <50596878+sekijohn10@users.noreply.github.com>
2019-11-06 19:21:30 -0700

    Added messages to event functions

--- Script3.js
+++ Script3.js
@@ -1,11 +1,29 @@
 function it1Ended() {
+    let el = document.getElementById("it1");
+    el.innerHTML = "Transition has Ended";
+    el.style.color = "red";
+}
 
+function it1Started() {
+    let el = document.getElementById("it1");
+    el.innerHTML = "Transition has Started";
+    el.style.color = "blue";
 }
 
 function it2Ended() {
-
+    let el = document.getElementById("it2");
+    el.innerHTML = "Animation has Ended";
+    el.style.fontFamily = "cursive";
+    el.style.color = "brown";
 }
 
 function it2Started() {
+    let el = document.getElementById("it2");
+    el.innerHTML = "Animation has Started";
+    el.style.fontFamily = "sans-serif";
+    el.style.color = "green";
+}
+
+function startAnimation() {
 
 }
